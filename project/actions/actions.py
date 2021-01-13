8b37605a5611ce577984f61d5fc2b81c194e1db9
Raaga Pranitha Kolla <raagapranithakolla@Raagas-MacBook-Pro.local>
2021-01-12 18:35:08 -0800

    Save values

--- project/actions/actions.py
+++ project/actions/actions.py
@@ -7,11 +7,11 @@
 
 # This is a simple example for a custom action which utters "Hello World!"
 
-# from typing import Any, Text, Dict, List
-#
-# from rasa_sdk import Action, Tracker
-# from rasa_sdk.executor import CollectingDispatcher
-#
+from typing import Any, Text, Dict, List
+
+from rasa_sdk import Action, Tracker
+from rasa_sdk.executor import CollectingDispatcher
+
 #
 # class ActionHelloWorld(Action):
 #
@@ -25,3 +25,15 @@
 #         dispatcher.utter_message(text="Hello World!")
 #
 #         return []
+class ActionSubmit(Action):
+
+    def name(self) -> Text:
+        return "action_submit_request"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+
+        dispatcher.utter_message(text="Your Name is {0}\n and your address is {1}\n your phone number is {2}\n your requested items are {3}\n".format(tracker.get_slot("name"),tracker.get_slot("address"),tracker.get_slot("mobile"),tracker.get_slot("items")))
+
+        return []
---
+++ project/actions/database_connection.py
@@ -0,0 +1,3 @@
+import mysql.connector
+mysql.connector.connect(host='localhost',database='mysql',user='root',password='root')
+# def dataUpdate(name,address,mobile,items):
